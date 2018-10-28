68c11afc8f5f31eff88c6c3ae253eccf62b0ec0c
Martin Lorenzo Tanchanco <tanchanco@gmail.com>
2018-10-28 18:48:00 +0000

    added https response to https_examples.js

--- https_examples.js
+++ https_examples.js
@@ -6,8 +6,9 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function () {
+var callback = function (response) {
   console.log('In response to handler callback!');
+  console.log('Response: ', response);
 };
 
 console.log("I'm about to make the request!");
